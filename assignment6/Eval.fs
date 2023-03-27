8c11ef05a407e8f8d664f19f67ce83b1870054ed
JonasUJ <jonas@ulsnes.dk>
2023-03-27 16:57:31 +0200

    Format and do some tasks

--- assignment6/Eval.fs
+++ assignment6/Eval.fs
@@ -1,117 +1,123 @@
 ﻿module Eval
 
-    open StateMonad
-    open Types
+open StateMonad
+open Types
 
-    (* Code for testing *)
+(* Code for testing *)
 
-    let hello = [(* INSERT YOUR DEFINITON OF HELLO HERE.*)] 
-    let state = mkState [("x", 5); ("y", 42)] hello ["_pos_"; "_result_"]
-    let emptyState = mkState [] [] []
-    
-    let add a b = failwith "Not implemented"      
-    let div a b = failwith "Not implemented"      
+let hello = [ ('H', 4); ('E', 1); ('L', 1); ('L', 1); ('O', 1) ]
+let state = mkState [ ("x", 5); ("y", 42) ] hello [ "_pos_"; "_result_" ]
+let emptyState = mkState [] [] []
 
-    type aExp =
-        | N of int
-        | V of string
-        | WL
-        | PV of aExp
-        | Add of aExp * aExp
-        | Sub of aExp * aExp
-        | Mul of aExp * aExp
-        | Div of aExp * aExp
-        | Mod of aExp * aExp
-        | CharToInt of cExp
+let add a b = failwith "Not implemented"
+let div a b = failwith "Not implemented"
 
-    and cExp =
-       | C  of char  (* Character value *)
-       | CV of aExp  (* Character lookup at word index *)
-       | ToUpper of cExp
-       | ToLower of cExp
-       | IntToChar of aExp
+type aExp =
+    | N of int
+    | V of string
+    | WL
+    | PV of aExp
+    | Add of aExp * aExp
+    | Sub of aExp * aExp
+    | Mul of aExp * aExp
+    | Div of aExp * aExp
+    | Mod of aExp * aExp
+    | CharToInt of cExp
 
-    type bExp =             
-       | TT                   (* true *)
-       | FF                   (* false *)
+and cExp =
+    | C of char (* Character value *)
+    | CV of aExp (* Character lookup at word index *)
+    | ToUpper of cExp
+    | ToLower of cExp
+    | IntToChar of aExp
 
-       | AEq of aExp * aExp   (* numeric equality *)
-       | ALt of aExp * aExp   (* numeric less than *)
+type bExp =
+    | TT (* true *)
+    | FF (* false *)
 
-       | Not of bExp          (* boolean not *)
-       | Conj of bExp * bExp  (* boolean conjunction *)
+    | AEq of aExp * aExp (* numeric equality *)
+    | ALt of aExp * aExp (* numeric less than *)
 
-       | IsVowel of cExp      (* check for vowel *)
-       | IsLetter of cExp     (* check for letter *)
-       | IsDigit of cExp      (* check for digit *)
+    | Not of bExp (* boolean not *)
+    | Conj of bExp * bExp (* boolean conjunction *)
 
-    let (.+.) a b = Add (a, b)
-    let (.-.) a b = Sub (a, b)
-    let (.*.) a b = Mul (a, b)
-    let (./.) a b = Div (a, b)
-    let (.%.) a b = Mod (a, b)
+    | IsVowel of cExp (* check for vowel *)
+    | IsLetter of cExp (* check for letter *)
+    | IsDigit of cExp (* check for digit *)
 
-    let (~~) b = Not b
-    let (.&&.) b1 b2 = Conj (b1, b2)
-    let (.||.) b1 b2 = ~~(~~b1 .&&. ~~b2)       (* boolean disjunction *)
-    let (.->.) b1 b2 = (~~b1) .||. b2           (* boolean implication *) 
-       
-    let (.=.) a b = AEq (a, b)   
-    let (.<.) a b = ALt (a, b)   
-    let (.<>.) a b = ~~(a .=. b)
-    let (.<=.) a b = a .<. b .||. ~~(a .<>. b)
-    let (.>=.) a b = ~~(a .<. b)                (* numeric greater than or equal to *)
-    let (.>.) a b = ~~(a .=. b) .&&. (a .>=. b) (* numeric greater than *)    
+let (.+.) a b = Add(a, b)
+let (.-.) a b = Sub(a, b)
+let (.*.) a b = Mul(a, b)
+let (./.) a b = Div(a, b)
+let (.%.) a b = Mod(a, b)
 
-    let arithEval a : SM<int> = failwith "Not implemented"      
+let (~~) b = Not b
+let (.&&.) b1 b2 = Conj(b1, b2)
 
-    let charEval c : SM<char> = failwith "Not implemented"      
+let (.||.) b1 b2 =
+    ~~(~~b1 .&&. ~~b2) (* boolean disjunction *)
 
-    let boolEval b : SM<bool> = failwith "Not implemented"
+let (.->.) b1 b2 =
+    (~~b1) .||. b2 (* boolean implication *)
+
+let (.=.) a b = AEq(a, b)
+let (.<.) a b = ALt(a, b)
+let (.<>.) a b = ~~(a .=. b)
+let (.<=.) a b = a .<. b .||. ~~(a .<>. b)
+
+let (.>=.) a b =
+    ~~(a .<. b) (* numeric greater than or equal to *)
+
+let (.>.) a b =
+    ~~(a .=. b) .&&. (a .>=. b) (* numeric greater than *)
+
+let arithEval a : SM<int> = failwith "Not implemented"
+
+let charEval c : SM<char> = failwith "Not implemented"
+
+let boolEval b : SM<bool> = failwith "Not implemented"
 
 
-    type stmnt =                  (* statements *)
-    | Declare of string           (* variable declaration *)
-    | Ass of string * aExp        (* variable assignment *)
-    | Skip                        (* nop *)
-    | Seq of stmnt * stmnt        (* sequential composition *)
+type stmnt = (* statements *)
+    | Declare of string (* variable declaration *)
+    | Ass of string * aExp (* variable assignment *)
+    | Skip (* nop *)
+    | Seq of stmnt * stmnt (* sequential composition *)
     | ITE of bExp * stmnt * stmnt (* if-then-else statement *)
-    | While of bExp * stmnt       (* while statement *)
+    | While of bExp * stmnt (* while statement *)
 
-    let rec stmntEval stmnt : SM<unit> = failwith "Not implemented"
+let rec stmntEval stmnt : SM<unit> = failwith "Not implemented"
 
 (* Part 3 (Optional) *)
 
-    type StateBuilder() =
+type StateBuilder() =
 
-        member this.Bind(f, x)    = f >>= x
-        member this.Return(x)     = ret x
-        member this.ReturnFrom(x) = x
-        member this.Delay(f)      = f ()
-        member this.Combine(a, b) = a >>= (fun _ -> b)
-        
-    let prog = new StateBuilder()
+    member this.Bind(f, x) = f >>= x
+    member this.Return(x) = ret x
+    member this.ReturnFrom(x) = x
+    member this.Delay(f) = f ()
+    member this.Combine(a, b) = a >>= (fun _ -> b)
 
-    let arithEval2 a = failwith "Not implemented"
-    let charEval2 c = failwith "Not implemented"
-    let rec boolEval2 b = failwith "Not implemented"
+let prog = new StateBuilder()
 
-    let stmntEval2 stm = failwith "Not implemented"
+let arithEval2 a = failwith "Not implemented"
+let charEval2 c = failwith "Not implemented"
+let rec boolEval2 b = failwith "Not implemented"
 
-(* Part 4 (Optional) *) 
+let stmntEval2 stm = failwith "Not implemented"
 
-    let stmntToSquareFun stm = failwith "Not implemented"
+(* Part 4 (Optional) *)
 
-    let stmntToBoardFun stm m = failwith "Not implemented"
+let stmntToSquareFun stm = failwith "Not implemented"
 
-    type squareStmnt = Map<int, stmnt>
-    let stmntsToSquare stms = failwith "Not implemented"
+let stmntToBoardFun stm m = failwith "Not implemented"
 
-    type board = {
-        center        : coord
-        defaultSquare : square
-        squares       : boardFun
-    }
+type squareStmnt = Map<int, stmnt>
+let stmntsToSquare stms = failwith "Not implemented"
 
-    let mkBoard c defaultSq boardStmnt ids = failwith "Not implemented"
-    
+type board =
+    { center: coord
+      defaultSquare: square
+      squares: boardFun }
+
+let mkBoard c defaultSq boardStmnt ids = failwith "Not implemented"
